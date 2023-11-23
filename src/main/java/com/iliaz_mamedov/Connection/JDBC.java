dba06b4fb827e8f84077db3f5dd43cf72d23c29c
iliaz <izmamedov4@mail.ru>
2023-11-23 23:26:00 +0300

    add Connection classes

---
+++ src/main/java/com/iliaz_mamedov/Connection/JDBC.java
@@ -0,0 +1,36 @@
+package com.iliaz_mamedov.Connection;
+
+import java.util.*;
+import java.sql.*;
+public class JDBC {
+    public static Connection connection = null;
+
+    public static void connect() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("MySQL JDBC Driver Registered");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Where is your MySQL JDBC Driver?");
+            e.printStackTrace();
+            throw new SQLException();
+        }
+        connection = DriverManager
+                .getConnection("jdbc:mysql://localhost/books?useUnicode=true&serverTimezone=UTC", "Iliaz", "qwe987RR!");
+        if (connection == null) {
+            throw new SQLException();
+        } else {
+            System.out.println("Successfully connected");
+        }
+    }
+
+    public static void close() {
+        try {
+            if (connection != null) {
+                connection.close();
+                System.out.println("Closing connection");
+            }
+        } catch (SQLException e) {
+            System.out.println("Failed to close connection");
+        }
+    }
+}
---
+++ src/main/java/com/iliaz_mamedov/Connection/TestDatabase.java
@@ -0,0 +1,21 @@
+package com.iliaz_mamedov.Connection;
+
+import com.iliaz_mamedov.Connection.JDBC;
+import java.util.*;
+import java.sql.*;
+public class TestDatabase {
+    public static void main(String[] argv) {
+        System.out.println("---- MySQL JDBC Connection Testing ----");
+
+        try {
+            JDBC.connect();
+        } catch (SQLException e) {
+            System.out.println("Connection Failed");
+            e.printStackTrace();
+            return;
+        }
+
+        JDBC.close();
+    }
+
+}
